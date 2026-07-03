ab2082b8c17156a2816042a67bddf6444f93d4b7
Zainab <zainabali0516@gmail.com>
2026-07-04 02:24:16 +0500

    upload task

---
+++ destination.js
@@ -0,0 +1,36 @@
+const destinations = [
+  {
+    id: 1,
+    placeName: "Hunza Valley",
+    country: "Pakistan",
+    budget: 35000,
+    duration: "5 Days",
+    season: "Spring",
+  },
+  {
+    id: 2,
+    placeName: "Bali",
+    country: "Indonesia",
+    budget: 80000,
+    duration: "6 Days",
+    season: "Summer",
+  },
+  {
+    id: 3,
+    placeName: "Paris",
+    country: "France",
+    budget: 150000,
+    duration: "7 Days",
+    season: "Winter",
+  },
+  {
+    id: 4,
+    placeName: "Skardu",
+    country: "Pakistan",
+    budget: 45000,
+    duration: "4 Days",
+    season: "Autumn",
+  },
+];
+
+export default destinations;
