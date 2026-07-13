116824ce80c2d0e8fe8f945fd6c49292c01b70f1
Zainab <zainabali0516@gmail.com>
2026-07-13 23:26:59 +0500

    Add files via upload

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Us Page</h1>
+      <p>We are learning routing, layouts, and dynamic parameters in Next.js!</p>
+    </div>
+  );
+}
