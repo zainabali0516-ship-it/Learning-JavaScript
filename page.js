3484a71d330e6c36cb8820ce528427e172e45943
Zainab <zainabali0516@gmail.com>
2026-07-13 23:31:06 +0500

    Add files via upload

--- page.js
+++ page.js
@@ -1,8 +1,12 @@
-export default function ServicesPage() {
+import Link from 'next/link';
+export default function StudentsPage() {
   return (
     <div>
-      <h1>Services Main Page</h1>
-      <p>Please select a service from the sidebar menu to see nested routing in action.</p>
+      <h1>Students List</h1>
+      <ul>
+        <li><Link href="/students/101">Student Roll No: 101</Link></li>
+        <li><Link href="/students/25">Student Roll No: 25</Link></li>
+      </ul>
     </div>
   );
 }
